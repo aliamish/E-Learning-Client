0584c6350b8fe209245132ac5f92d91dcbec3e41
Talha Raza <talha.raza@eventcombo.com>
2025-10-27 12:27:48 +0500

    done

--- app/components/Admin/DashboardHeader.tsx
+++ app/components/Admin/DashboardHeader.tsx
@@ -3,62 +3,72 @@ import React, { FC, useEffect, useState } from "react";
 import { ThemeSwitcher } from "@/app/utils/ThemeSwitcher";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { useTheme } from "next-themes";
-import socketIo from "socket.io-client"
-import { useGetAllNotificationsQuery, useUpdateNotificationsStatusMutation } from "@/redux/features/notifications/notificationsApi";
+import socketIo from "socket.io-client";
+import {
+  useGetAllNotificationsQuery,
+  useUpdateNotificationsStatusMutation,
+} from "@/redux/features/notifications/notificationsApi";
 import { format } from "path";
+
 const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
 const socketId = socketIo(ENDPOINT, { transports: ["websocket"] });
 
 type Props = {
   open?: boolean;
-  setOpen?:any;
+  setOpen?: any;
 };
 
-const DashboardHeader: FC<Props> = ({open,setOpen}) => {
-  const {data, refetch } = useGetAllNotificationsQuery(undefined, {
+const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
+  const { data, refetch } = useGetAllNotificationsQuery(undefined, {
     refetchOnMountOrArgChange: true,
   });
 
-  const [updateNotificationsStatus,{isSuccess}] = useUpdateNotificationsStatusMutation();
+  const [updateNotificationsStatus, { isSuccess }] =
+    useUpdateNotificationsStatusMutation();
   const [notifications, setNotifications] = useState<any>([]);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const { theme } = useTheme();
 
-  const [audio] = useState(
-    new Audio(
-      "https://res.cloudinary.com/damk25wo5/video/upload/v1693465789/notification_vectjn.mp3"
-    )
-  );
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const newAudio = new Audio(
+        "https://res.cloudinary.com/damk25wo5/video/upload/v1693465789/notification_vectjn.mp3"
+      );
+      setAudio(newAudio);
+    }
+  }, []);
 
   const playNotificationSound = () => {
-    audio.play();
+    if (audio) {
+      audio.play();
+    }
   };
 
   useEffect(() => {
-    if(data){
-       setNotifications(
+    if (data) {
+      setNotifications(
         data.notifications.filter((item: any) => item.status === "unread")
-       )
+      );
     }
-    if(isSuccess){
+    if (isSuccess) {
       refetch();
     }
-    audio.load();
-
-  },[data,isSuccess]);
-
+    if (audio) {
+      audio.load();
+    }
+  }, [data, isSuccess, audio]);
 
   useEffect(() => {
     socketId.on("newNotification", (data) => {
       refetch();
-      playNotificationSound()
-    })
-  },[])
+      playNotificationSound();
+    });
+  }, [audio]);
 
   const handleNotificationStatusChange = async (id: string) => {
-    await updateNotificationsStatus(id)
-  }
+    await updateNotificationsStatus(id);
+  };
 
-  
-  const {theme} = useTheme()
   return (
     <div className="w-full flex items-center justify-end p-6 fixed top-5 right-0 ">
       <ThemeSwitcher />
@@ -66,42 +76,74 @@ const DashboardHeader: FC<Props> = ({open,setOpen}) => {
         className="relative cursor-pointer m-2 "
         onClick={() => setOpen(!open)}
       >
-        <IoMdNotificationsOutline className={`text-2xl cursor-pointer ${theme === "dark" ? "text-white" : "text-black"}`} />
+        <IoMdNotificationsOutline
+          className={`text-2xl cursor-pointer ${
+            theme === "dark" ? "text-white" : "text-black"
+          }`}
+        />
         <span className="absolute -top-2 -right-2 bg-[#3ccba0] rounded-full w-[20px] h-[20px] text-[12px] flex items-center justify-center text-white ">
           {notifications && notifications.length}
         </span>
-              </div>
+      </div>
 
-        {open && (
-          <div className={`w-[350px] h-[50vh] ${theme === "dark" ? "dark:bg-[#111C43]" : "bg-white"} shadow-xl absolute top-16 z-10 rounded  `}>
-            <h5 className={`text-center text-[20px] font-Poppins ${theme === "dark" ? "text-white" : "text-black bg-white"} p-3`}>
-              Notification
-            </h5>
-           {
-            notifications && notifications.map((item:any, index:number) => {
-               <div className={`dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]`}>
-              <div className="w-full flex items-center justify-between p-2">
-                <p className={`${theme === "dark" ? "text-white" : "text-black bg-white"}`}>
-                  {item.title}
-                </p>
-                <p className={`${theme === "dark" ? "text-white" : "text-black bg-white"} cursor-pointer`}
-                onClick={() => handleNotificationStatusChange(item._id)}
+      {open && (
+        <div
+          className={`w-[350px] h-[50vh] ${
+            theme === "dark" ? "dark:bg-[#111C43]" : "bg-white"
+          } shadow-xl absolute top-16 z-10 rounded`}
+        >
+          <h5
+            className={`text-center text-[20px] font-Poppins ${
+              theme === "dark" ? "text-white" : "text-black bg-white"
+            } p-3`}
+          >
+            Notification
+          </h5>
+          {notifications &&
+            notifications.map((item: any, index: number) => (
+              <div
+                key={index}
+                className={`dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]`}
+              >
+                <div className="w-full flex items-center justify-between p-2">
+                  <p
+                    className={`${
+                      theme === "dark"
+                        ? "text-white"
+                        : "text-black bg-white"
+                    }`}
+                  >
+                    {item.title}
+                  </p>
+                  <p
+                    className={`${
+                      theme === "dark"
+                        ? "text-white"
+                        : "text-black bg-white"
+                    } cursor-pointer`}
+                    onClick={() => handleNotificationStatusChange(item._id)}
+                  >
+                    Mark as read
+                  </p>
+                </div>
+                <p
+                  className={`${
+                    theme === "dark" ? "text-white" : "text-black bg-white"
+                  }`}
                 >
-                  Mark as read
+                  {item.message}
+                </p>
+                <p
+                  className={`${
+                    theme === "dark" ? "text-white" : "text-black bg-white"
+                  } text-[14px]`}
+                >
+                  {format(item.createdAt)}
                 </p>
               </div>
-              <p className={`${theme === "dark" ? "text-white" : "text-black bg-white"}`}>
-               {item.message}
-              </p>
-              <p className={`${theme === "dark" ? "text-white" : "text-black bg-white"} text-[14px]`}>
-                {format(item.createdAt)}
-              </p>
-            </div>
-            })
-           }
-           
-          </div>
-        )}
+            ))}
+        </div>
+      )}
     </div>
   );
 };
