2680d55187a5f68804516572b750843944b5a991
Talha Raza <talha.raza@eventcombo.com>
2025-10-27 11:10:56 +0500

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 "use client";
 import "./globals.css";
 import React, { useEffect } from "react";
-import { Poppins } from "next/font/google";
-import { Josefin_Sans } from "next/font/google";
 import { ThemeProvider } from "./utils/theme-provider";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
@@ -14,17 +12,6 @@ const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
 const socketId = socketIo(ENDPOINT, { transports: ["websocket"] });
 
 
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins", // ✅ correct spelling
-});
-
-const josefin = Josefin_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-josefin", // ✅ correct spelling
-});
 
 export default function RootLayout({
   children,
@@ -34,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} ${josefin.variable}  bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
+        className={` bg-no-repeat dark:bg-gradient-to-b dark:from-gray-900 dark:to-black duration-300`}
       >
         <Providers>
           <SessionProvider>
